411fc003c05937b56f992a3d77849d919ee941cc
seeyoujeong <chan3358@naver.com>
2024-02-20 23:23:45 +0900

    feat: createApiClient 함수 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,64 @@
 import { API_ENDPOINT_URL, API_HEADER_X_USERNAME } from "../constants";
 
-export const baseApi = async (path: string, options?: RequestInit) => {
+const fetchWrapper = async (url: string, options?: RequestInit) => {
   try {
-    const response = await fetch(`${API_ENDPOINT_URL}${path}`, {
-      ...options,
-      headers: {
-        ...options?.headers,
-        "Content-Type": "application/json",
-        "x-username": API_HEADER_X_USERNAME,
-      },
-    });
+    const response = await fetch(url, options);
 
     if (response.ok) {
       return await response.json();
     }
   } catch (error) {
-    console.error(error);
+    console.error("fetchWrapper: ", error);
   }
 };
+
+const concatUrlPath = (url: string, path: string) => {
+  if (url[url.length - 1] !== "/") {
+    url += "/";
+  }
+
+  if (path[0] === "/") {
+    path = path.slice(1);
+  }
+
+  return `${url}${path}`;
+};
+
+type OptionBody = BodyInit | undefined | null;
+
+const createApiClient = (url: string, options?: RequestInit) => {
+  return {
+    get: async (path: string) => {
+      return await fetchWrapper(concatUrlPath(url, path), {
+        ...options,
+        method: "GET",
+      });
+    },
+    post: async (path: string, body: OptionBody) => {
+      return await fetchWrapper(concatUrlPath(url, path), {
+        ...options,
+        method: "POST",
+        body: JSON.stringify(body),
+      });
+    },
+    put: async (path: string, body: OptionBody) => {
+      return await fetchWrapper(concatUrlPath(url, path), {
+        ...options,
+        method: "PUT",
+        body: JSON.stringify(body),
+      });
+    },
+    delete: async (path: string) => {
+      return await fetchWrapper(concatUrlPath(url, path), {
+        method: "DELETE",
+      });
+    },
+  };
+};
+
+export const apiClient = createApiClient(API_ENDPOINT_URL, {
+  headers: {
+    "Content-Type": "application/json",
+    "x-username": API_HEADER_X_USERNAME,
+  },
+});
