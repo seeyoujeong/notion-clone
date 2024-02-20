3d21551d95e01749c11783c161c435bff8220492
seeyoujeong <chan3358@naver.com>
2024-02-20 19:49:02 +0900

    refactor: 폴더명 수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import { API_ENDPOINT_URL, API_HEADER_X_USERNAME } from "../constants";
 
-export const baseApi = async (path: string, options: RequestInit) => {
+export const baseApi = async (path: string, options?: RequestInit) => {
   try {
     const response = await fetch(`${API_ENDPOINT_URL}${path}`, {
       ...options,
       headers: {
-        ...options.headers,
+        ...options?.headers,
         "Content-Type": "application/json",
         "x-username": API_HEADER_X_USERNAME,
       },
