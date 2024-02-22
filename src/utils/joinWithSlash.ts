c7f19164e8d1f391239e334d9e9da10e0682a600
seeyoujeong <chan3358@naver.com>
2024-02-23 00:39:30 +0900

    refactor: joinWithSlash 함수 수정

--- src/utils/joinWithSlash.ts
+++ src/utils/joinWithSlash.ts
@@ -1,8 +1,17 @@
-export const joinWithSlash = (...args: (number | string)[]) => {
-  return (
-    args
-      .map(String)
-      .map((str) => str.replace(/^\/|\/$/g, ""))
-      .join("/") + "/"
-  );
+const joinSlash = (array: (number | string)[]) => {
+  return array
+    .map(String)
+    .map((str) => str.replace(/^\/|\/$/g, ""))
+    .join("/");
+};
+
+export const joinWithSlash = (
+  isLastOrArg: boolean | number | string,
+  ...args: (number | string)[]
+) => {
+  if (typeof isLastOrArg === "boolean") {
+    return joinSlash(args) + (isLastOrArg ? "/" : "");
+  }
+
+  return joinSlash([isLastOrArg, ...args]);
 };
