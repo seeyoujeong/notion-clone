e005d66060e7081445e394815b75d59294c876f6
seeyoujeong <chan3358@naver.com>
2024-02-25 14:34:57 +0900

    feat: Home 컴포넌트 추가

---
+++ src/pages/Home.ts
@@ -0,0 +1,12 @@
+import { Component } from "@/core";
+
+class Home extends Component {
+  template(): string {
+    return `
+      <aside></aside>
+      <section></section>
+    `;
+  }
+}
+
+export default Home;
