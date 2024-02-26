a038cc6333a2287e26c19d992a9d5deb01f2ec2f
seeyoujeong <chan3358@naver.com>
2024-02-26 15:34:02 +0900

    feat: createRouter 함수 추가

---
+++ src/core/createRouter.ts
@@ -0,0 +1,47 @@
+import { Component } from ".";
+
+interface RouteObject {
+  path: string;
+  page: typeof Component;
+}
+
+const isSameSegments = (segments1: string[], segments2: string[]) => {
+  if (segments1.length !== segments2.length) {
+    return false;
+  }
+
+  return segments1.every((seg, idx) => {
+    if (seg === segments2[idx]) {
+      return true;
+    } else if (seg.startsWith(":")) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+};
+
+const isSamePath = (routePath: string, currentPath: string) => {
+  if (routePath === currentPath) return true;
+
+  const routePathSegments = routePath.split("/");
+  const currentPathSegments = currentPath.split("/");
+
+  return isSameSegments(routePathSegments, currentPathSegments);
+};
+
+const createRouter = (routes: RouteObject[]) => {
+  return (currentPath: string, targetEl: HTMLElement) => {
+    const matchedRoute = routes.find(({ path }) =>
+      isSamePath(path, currentPath)
+    );
+
+    if (matchedRoute) {
+      new matchedRoute.page({ targetEl });
+    } else {
+      console.error("no matched route");
+    }
+  };
+};
+
+export default createRouter;
