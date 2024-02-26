baa813b31d7f1028ca7cd45997f9a7ea2517379a
seeyoujeong <chan3358@naver.com>
2024-02-26 15:36:07 +0900

    feat: Detail 페이지 추가

---
+++ src/pages/Detail.ts
@@ -0,0 +1,49 @@
+import { DocumentEdit, DocumentList } from "@/components";
+import { Component } from "@/core";
+import { deleteDocument, getAllDocuments, postDocument } from "@/domain";
+
+class Detail extends Component {
+  template(): string {
+    return `
+      <aside></aside>
+      <section></section>
+    `;
+  }
+
+  mounted(): void {
+    const documentList = new DocumentList({
+      targetEl: document.querySelector("aside")!,
+      state: [
+        {
+          id: 0,
+          title: "test",
+          documents: [],
+        },
+      ],
+      props: {
+        addDocument: async (parentId: number | null) => {
+          await postDocument("새 제목", parentId);
+
+          documentList.setState(await getAllDocuments());
+        },
+        deleteDocument: async (id: number) => {
+          await deleteDocument(id);
+
+          documentList.setState(await getAllDocuments());
+        },
+      },
+    });
+
+    new DocumentEdit({
+      targetEl: document.querySelector("section")!,
+    });
+
+    (async () => {
+      const list = await getAllDocuments();
+
+      documentList.setState(list);
+    })();
+  }
+}
+
+export default Detail;
