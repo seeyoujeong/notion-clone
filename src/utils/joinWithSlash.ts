859eb06a7e048443b30f0d4844d1bc75d9a1aea8
seeyoujeong <chan3358@naver.com>
2024-02-27 20:58:11 +0900

    refactor: export default로 수정

--- src/utils/joinWithSlash.ts
+++ src/utils/joinWithSlash.ts
@@ -1,6 +1,8 @@
-export const joinWithSlash = (...args: (number | string)[]) => {
+const joinWithSlash = (...args: (number | string)[]) => {
   return args
     .map(String)
     .map((str) => str.replace(/^\/|\/$/g, ""))
     .join("/");
 };
+
+export default joinWithSlash;
