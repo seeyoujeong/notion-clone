bfa1e4381a12c837efc32acdaab15aa525fce769
seeyoujeong <chan3358@naver.com>
2024-02-28 17:59:03 +0900

    feat: storage 추가

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export { default as createApiClient } from "./createApiClient";
 export * from "./generateElement";
 export { default as browserHistory } from "./browserHistory";
+export { default as storage } from "./storage";
---
+++ src/services/storage.ts
@@ -0,0 +1,23 @@
+interface StorageProps<V> {
+  _storage: Storage;
+  key: string;
+  defaultValue: V;
+}
+
+const storage = <V>({ _storage, key, defaultValue }: StorageProps<V>) => ({
+  setItem(value: V) {
+    _storage.setItem(key, JSON.stringify(value));
+  },
+  getItem() {
+    if (!_storage.getItem(key)) {
+      this.setItem(defaultValue);
+    }
+
+    return JSON.parse(_storage.getItem(key)!);
+  },
+  removeItem() {
+    _storage.removeItem(key);
+  },
+});
+
+export default storage;
