29994fa2553752541445eec09ef527808e568068
seeyoujeong <chan3358@naver.com>
2024-02-28 10:40:28 +0900

    feat: browserHistory 객체 추가

---
+++ src/services/browserHistory.ts
@@ -0,0 +1,55 @@
+interface HistoryEventDetail {
+  nextUrl: string;
+  option: "push" | "replace";
+}
+
+const HISTORY_EVENT_NAME = "historyEvent";
+const PUSH = "push";
+const REPLACE = "replace";
+
+const browserHistory = {
+  init(callback: () => void) {
+    window.addEventListener("popstate", () => callback());
+
+    window.addEventListener(
+      "history",
+      (event: CustomEventInit<HistoryEventDetail>) => {
+        const { nextUrl, option } = event.detail!;
+
+        if (nextUrl) {
+          if (option === PUSH) {
+            history.pushState(null, "", nextUrl);
+            callback();
+          }
+
+          if (option === REPLACE) {
+            history.replaceState(null, "", nextUrl);
+            callback();
+          }
+        }
+      }
+    );
+  },
+  push(nextUrl: string) {
+    window.dispatchEvent(
+      new CustomEvent(HISTORY_EVENT_NAME, {
+        detail: {
+          nextUrl,
+          option: PUSH,
+        },
+      })
+    );
+  },
+  replace(nextUrl: string) {
+    window.dispatchEvent(
+      new CustomEvent(HISTORY_EVENT_NAME, {
+        detail: {
+          nextUrl,
+          option: REPLACE,
+        },
+      })
+    );
+  },
+};
+
+export default browserHistory;
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export { default as createApiClient } from "./createApiClient";
 export * from "./generateElement";
+export { default as browserHistory } from "./browserHistory";
