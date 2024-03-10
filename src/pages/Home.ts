e04f87e34164ad070eb2e7c38532b2f70112fc53
seeyoujeong <chan3358@naver.com>
2024-03-10 21:31:12 +0900

    feat: Home 페이지 내용 추가

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -3,7 +3,10 @@ import { Component } from "@/core";
 class Home extends Component {
   template(): string {
     return `
-      Notion Home
+      <div class="home">
+        <h1>Hi there  👋</h1>
+        <h2>노션 클로닝 with TS</h2>
+      </div>
     `;
   }
 }
