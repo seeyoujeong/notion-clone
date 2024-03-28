8c3208dda99944f1be9271087dc98d9b6a67e099
seeyoujeong <chan3358@naver.com>
2024-03-28 14:42:30 +0900

    refactor: addIsToggledToDocuments 함수 수정

--- src/utils/addIsToggledToDocuments.ts
+++ src/utils/addIsToggledToDocuments.ts
@@ -1,14 +1,17 @@
-import { toggledStorage } from "@/domain";
 import { DocumentListContent, RootDocument } from "@/types";
 
 const addIsToggledToDocuments = (
-  documents: RootDocument[]
+  documents: RootDocument[],
+  toggledList: number[]
 ): DocumentListContent[] => {
   return documents.map(({ id, title, documents }) => ({
     id,
     title,
-    isToggled: toggledStorage.has(id),
-    documents: documents.length > 0 ? addIsToggledToDocuments(documents) : [],
+    isToggled: toggledList.includes(id),
+    documents:
+      documents.length > 0
+        ? addIsToggledToDocuments(documents, toggledList)
+        : [],
   }));
 };
 
