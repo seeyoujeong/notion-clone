fde94af1ea069e49bae31fa28abd9f6be6ac9f32
seeyoujeong <chan3358@naver.com>
2024-05-21 13:24:11 +0900

    feat: 홈 화면 내용 추가

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -6,6 +6,13 @@ class Home extends Component {
       <div class="home">
         <h1>Hi there  👋</h1>
         <h2>노션 클로닝 with TS</h2>
+
+        <h3>사용 가능한 명령어 목록</h3>
+        <ul>
+          <li># + space : 제목 1</li>
+          <li>## + space : 제목 2</li>
+          <li>### + space : 제목 3</li>
+        </ul>
       </div>
     `;
   }
