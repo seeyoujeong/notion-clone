71f1ff37deea1aa8cb9cba2955aef25ae909fb4e
seeyoujeong <seeyoujeong.dev@gmail.com>
2024-12-05 17:37:17 +0900

    feat: 오프라인 모드 표시 추가

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -6,6 +6,7 @@ class Home extends Component {
       <div class="home">
         <h1>Hi there  👋</h1>
         <h2>노션 클로닝 with TS</h2>
+        <h3>🔴 <span style="color: red;">현재 오프라인 모드</span> 🔴</h3>
 
         <h3>사용 가능한 명령어 목록</h3>
         <ul>
